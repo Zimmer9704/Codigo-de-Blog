52aaf4f06d3242a57d59786239674e9b1534046c
Zimmer9704 <42560679+Zimmer9704@users.noreply.github.com>
2018-08-31 12:50:06 -0500

    Blog v1.1
    Se modificaron las tablas de Publicacion, Categoria e Imagen

--- Codigo Blog.sql
+++ Codigo Blog.sql
@@ -1,16 +1,21 @@
 /*DROP  TABLE Archivo
 DROP  TABLE Artículo
 DROP  TABLE Usuarios*/
-
-
-CREAR  TABLA  Usuarios (
+CREATE  TABLE  Usuarios (
 	UsuarioID nvarchar ( 100 ) PRIMARY KEY ,
 	Contraseña nvarchar ( 100 ) NOT NULL ,
 	Nombre nvarchar ( 200 ),
-	Correo electrónico nvarchar ( 500 ),
+	Correo_electrónico varchar ( 500 ),
 	Administrador BIT  NOT NULL DEFAULT 0 
 	
 )
+
+CREATE TABLE  Categoria (
+	IDCategoria bigint  NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,	
+	Nombre nvarchar(50),
+	Etiquetas nvarchar(50)
+)
+
 
 CREATE  TABLE  Publicacion (
 	IDpublicacion bigint  NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,	
@@ -19,23 +24,11 @@ CREATE  TABLE  Publicacion (
 		
 	Texto Text NOT NULL ,
 	Titulo Text  NOT NULL ,	
-	Categoria nvarchar(50)		
+	IDCategoria bigint REFERENCES Categoria ,
+	Etiquetas varchar (50)	
 )
-CREATE TABLE  Categoria (
-	IDCategoria bigint  NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,	
-	Nombre nvarchar(50),
-	NombreArchivo nvarchar ( 500 ),	
-	Imagen de contenido,	
-	Etiquetas nvarchar(50)
-)
-
-CREATE TABLE  Etiqueta (
-	IDEtiqueta bigint  NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,	
-	Tag nvarchar(50),
-	link nvarchar ( 500 )
-)
-
-CREATE TABLE  Enlace (
-	IDlink bigint  NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,	
-	link nvarchar(50)
+CREATE TABLE Imagen (
+	IDImagen bigint NOT NULL IDENTITY ( 1 , 1 ) PRIMARY KEY ,
+	Imagen IMAGE,
+	IDpublicacion bigint REFERENCES Publicacion
 )
